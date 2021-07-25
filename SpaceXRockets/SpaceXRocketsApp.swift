a6289223af3d52cbb41350b99f49551db6b6a92a
batuhanbaran <fb_batuhan_baran@hotmail.com>
2021-07-25 14:24:27 +0300

    Initial Commit

---
+++ SpaceXRockets/SpaceXRocketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXRocketsApp.swift
+//  SpaceXRockets
+//
+//  Created by Batuhan Baran on 25.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXRocketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
